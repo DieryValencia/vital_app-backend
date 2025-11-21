2e0be5f63d3065a2ed2c053e85d3fdc4f3e94ff0
DieryValencia <diery.valencia@campusucc.edu.co>
2025-11-21 00:55:07 -0500

    refactor

--- src/main/java/com/vitalapp/vital_app_backend/service/PatientService.java
+++ src/main/java/com/vitalapp/vital_app_backend/service/PatientService.java
@@ -5,6 +5,8 @@ import java.time.Period;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
